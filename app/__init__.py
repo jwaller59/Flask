71f74ea66dca66c4189d9eaeea918efa5768612c
Jake Waller <55753338+jwaller59@users.noreply.github.com>
2020-07-05 16:04:46 +0100

    New changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
+Bootstrap = Bootstrap(app)
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,29 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    
+    return render_template('index.html', title='Home')
 
 @app.route('/main')
 def main():
     return 'Goodbye World'
+
+@app.route('/pack')
+def pack():
+    return render_template('Pack.html', title='Jarvis - Pack')
+
+@app.route('/pick')
+def pick():
+    return render_template('Pick.html', title='Jarvis - Pick')
+
+@app.route('/stow')
+def stow():
+    return render_template('Stow.html', title='Jarvis - Stow')
+
+@app.route('/receive')
+def receive():
+    return render_template('Receive.html', title='Jarvis - Receive')
+
